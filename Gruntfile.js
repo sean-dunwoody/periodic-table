d248289752fbd93f2137f8161834807164001620
Sean Dunwoody <sean_dunwoody@yahoo.com>
2015-07-27 00:09:34 +0100

    modified file structure (so site assets e.g. css, js and images, are in an 'assets' folder). Added in html minification to grunt, and cleaned up watch task so it runs more efficiently. Broke some CSS out into components owing to uncss removing some css that is necessary due to changes in javascript states (all css that is not dependant on javascript states resides in screen.css for the moment, although this will need changing as it's not a reliable approach)

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,41 +6,76 @@ module.exports = function(grunt) {
 
         concat: {
             // 2. Configuration for concatinating files goes here.
-            basic: {
+            js: {
                 src: [
-                    'js/ajax.js',
-                    'js/responsive-nav.js',
-                    'js/simple_tabs.js',
-                    'js/element.js',
-                    'js/default.js'
+                    'assets/js/ajax.js',
+                    'assets/js/responsive-nav.js',
+                    'assets/js/simple_tabs.js',
+                    'assets/js/element.js',
+                    'assets/js/default.js'
                 ],
-                dest: 'dist/js/default.concat.js'
+                dest: 'dist/assets/js/default.js'
+            },
+            // move the css file to the dist folder so we can run uncss on it
+            css: {
+                src: 'assets/css/screen.css',
+                dest: 'dist/assets/css/screen.css'
+            },
+            cssComponents: {
+                src: ['assets/css/components/_element.css', 'assets/css/components/_navigation.css'],
+                dest: 'dist/assets/css/components.css'
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    // includes files within path
+                    {
+                        expand: true,
+                        src: ['element/*.json'],
+                        dest: 'dist',
+                        filter: 'isFile'
+                    }
+                ]
             }
         },
         uglify: {
             build: {
-                src: 'dist/js/default.concat.js',
-                dest: 'dist/js/default.min.js'
+                src: 'dist/assets/js/default.js',
+                dest: 'dist/assets/js/default.min.js'
             }
         },
         cssmin: {
-            minify: {
-                expand: true,
-                cwd: 'css',
-                src: ['*.css', '!*.min.css'],
-                dest: 'dist/css',
-                ext: '.min.css'
+            target: {
+                files: {
+                    'dist/assets/css/screen.min.css': ['dist/assets/css/screen.css', 'dist/assets/css/components.css']
+                }
+            }
+        },
+        uncss: {
+            dist: {
+                files: {
+                    'dist/assets/css/screen.css': ['dist/index.html', 'dist/about.html']
+                }
             }
         },
         imagemin: {
-            dynamic: {
+            dist: {
                 files: [{
                     expand: true,
-                    cwd: 'images',
+                    cwd: 'assets/img',
                     flatten: true,
                     src: ['*.{png,jpg,jpeg,gif}'],
-                    dest: 'dist/images'
+                    dest: 'dist/assets/img'
                 }]
+            }
+        },
+        processhtml: {
+            dist: {
+                files: {
+                    'dist/index.html': ['index.html'],
+                    'dist/about.html': ['about.html'],
+                }
             }
         },
         htmlmin: {
@@ -50,32 +85,43 @@ module.exports = function(grunt) {
                     collapseWhitespace: true
                 },
                 files: {                                   // Dictionary of files
-                    'dist/index.html': 'index.html',     // 'destination': 'source'
-                    'dist/about.html': 'about.html'
+                    'dist/index.html': 'dist/index.html',     // 'destination': 'source'
+                    'dist/about.html': 'dist/about.html'
                 }
             }
         },
         watch: {
+            html: {
+                files: ['*.html'],
+                tasks: ['processhtml', 'htmlmin']
+            },
             css: {
-                files: ['css/*.css'],
-                tasks: ['cssmin'],
+                files: ['assets/css/**/*.css'],
+                tasks: ['concat', 'uncss', 'cssmin'],
                 options: {
                     livereload: true,
                 },
             },
+            js: {
+                files: ['assets/js/*.js'],
+                tasks: ['concat', 'uglify']
+            }
         }
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-uncss');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-processhtml');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'imagemin', 'htmlmin']);
+    grunt.registerTask('default', ['copy', 'concat', 'uncss', 'uglify', 'processhtml', 'htmlmin', 'cssmin', 'imagemin']);
 
 };
