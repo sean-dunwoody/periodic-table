a3d267357d50ac1cac688b8239f828d74d1337e2
Sean Dunwoody <sean_dunwoody@yahoo.com>
2015-08-07 23:22:40 +0100

    added primitive search functionality

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,8 +1,10 @@
 var navigationContainer = document.getElementById("navigation-container-inner");
 var searchInput = document.createElement("input");
 
+var elements = document.getElementsByClassName("el");
 var elementHeadings = document.getElementsByClassName("heading");
 headingNumber = elementHeadings.length;
+var elementNumber = elements.length;
 
 
 searchInput.setAttribute("type", "search");
@@ -14,11 +16,26 @@ searchInput.addEventListener("input", function(event) {
 	if(searchInput.value.length > 0) {
 		document.body.classList.add("search-active");
 		
-		for(var i = 0; i < (headingNumber - 1); i++) {
-			if(elementHeadings[i].innerHTML.toLowerCase().indexOf(searchInput.value.toLowerCase()) !== -1) {
-				elementHeadings[i].parentNode.classList.add("matched");
+		for(var i = 0; i < (elementNumber - 1); i++) {
+			var symbol  = elements[i].getElementsByClassName("heading");
+			var heading = elements[i].getElementsByClassName("heading-full");
+			var matched = false;
+
+			if(symbol[0]) {
+				if(symbol[0].innerHTML.toLowerCase().indexOf(searchInput.value.toLowerCase()) !== -1) {
+					matched = true;
+				}
+			}
+			if(heading[0]) {
+				if(heading[0].innerHTML.toLowerCase().indexOf(searchInput.value.toLowerCase()) !== -1) {
+					matched = true;
+				}
+			}
+
+			if(matched === true) {
+				elements[i].classList.add("matched");
 			} else {
-				elementHeadings[i].parentNode.classList.remove("matched");
+				elements[i].classList.remove("matched");
 			}
 		}
 	} else {
