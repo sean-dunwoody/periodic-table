8cc573e359ef554c70e03b299bae88597975c6ff
Sean Dunwoody <sean_dunwoody@yahoo.com>
2015-09-20 16:55:34 +0100

    Added a JavaScript function to check the X and Y position of elements

---
+++ assets/js/offset.js
@@ -0,0 +1,11 @@
+function findPos(obj) {
+    var curleft = 0, curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curleft += obj.offsetLeft;
+            curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+        return { x: curleft, y: curtop };
+    }
+    return undefined;
+}
